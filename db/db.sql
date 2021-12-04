970bd652a7f3e04ad70ed3c361620db38d013d99
Dmitry Varin <varindv@gmail.com>
2021-12-04 23:41:35 +0300

    add new sql-description of tables

--- db/db.sql
+++ db/db.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users
 (
     id       bigserial,
-    nickname citext NOT NULL unique primary key,
+    nickname citext NOT NULL UNIQUE PRIMARY KEY,
     fullname text   NOT NULL,
     about    text,
     email    citext NOT NULL UNIQUE
@@ -15,6 +15,43 @@ CREATE TABLE IF NOT EXISTS forum
     title          text   NOT NULL,
     users_nickname citext NOT NULL REFERENCES users (nickname),
     slug           citext NOT NULL PRIMARY KEY UNIQUE,
-    posts          bigint default 0,
-    threads        int    default 0
+    posts          bigint DEFAULT 0,
+    threads        int    DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS thread
+(
+    id      bigserial PRIMARY KEY NOT NULL UNIQUE,
+    title   text                  NOT NULL,
+    author  citext                NOT NULL REFERENCES users (nickname),
+    forum   citext                NOT NULL REFERENCES forum (slug),
+    message text                  NOT NULL,
+    votes   integer                  DEFAULT 0,
+    slug    citext,
+    created timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS post
+(
+    id        bigserial PRIMARY KEY NOT NULL UNIQUE,
+    parent    int                      DEFAULT 0,
+    author    citext                NOT NULL REFERENCES users (nickname),
+    message   text                  NOT NULL,
+    is_edited bool                     DEFAULT FALSE,
+    forum     citext REFERENCES forum (slug),
+    thread    integer REFERENCES thread (id),
+    created   timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS vote
+(
+    nickname  citext NOT NULL UNIQUE REFERENCES users (nickname),
+    thread_id int    NOT NULL UNIQUE REFERENCES thread (id),
+    voice     int    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_forum
+(
+    nickname citext NOT NULL UNIQUE REFERENCES  users (nickname),
+    forum    citext NOT NULL UNIQUE REFERENCES forum (slug)
 );
