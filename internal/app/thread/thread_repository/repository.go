781cf9c9d1d1b3285af55df79f7b2636682fa65d
Dmitry Varin <varindv@gmail.com>
2022-01-01 15:16:22 +0300

    add get methods in thread repository

--- internal/app/thread/thread_repository/repository.go
+++ internal/app/thread/thread_repository/repository.go
@@ -1,6 +1,10 @@
 package repository
 
+import "tp-db-project/internal/app/thread/models"
+
 //go:generate mockgen -destination=mocks/repository.go -package=mock_thread -mock_names=Repository=ThreadRepository . Repository
 
 type Repository interface {
+	GetByID(id int64) (*models.ResponseThread, error)
+	GetBySlug(slug string) (*models.ResponseThread, error)
 }
--- internal/app/thread/thread_repository/repository/thread_repository.go
+++ internal/app/thread/thread_repository/repository/thread_repository.go
@@ -1,6 +1,16 @@
 package thread_repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"tp-db-project/internal/app/thread/models"
+)
+
+const (
+	queryGetThreadById = "SELECT id, title, author, forum, message, votes, slug, created FROM thread " +
+		"WHERE id = $1"
+	queryGetThreadBySlug = "SELECT id, title, author, forum, message, votes, slug, created FROM thread " +
+		"WHERE slug = $1"
+)
 
 type ThreadRepository struct {
 	conn *sql.DB
@@ -10,4 +20,28 @@ func NewThreadRepository(conn *sql.DB) *ThreadRepository {
 	return &ThreadRepository{
 		conn: conn,
 	}
+}
+
+func (r *ThreadRepository) GetByID(id int64) (*models.ResponseThread, error) {
+	res := &models.ResponseThread{}
+
+	if err := r.conn.QueryRow(queryGetThreadById, id).
+		Scan(&res.Id, &res.Title, &res.Author, &res.Forum,
+			&res.Message, &res.Votes, &res.Slug, &res.Created); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (r *ThreadRepository) GetBySlug(slug string) (*models.ResponseThread, error) {
+	res := &models.ResponseThread{}
+
+	if err := r.conn.QueryRow(queryGetThreadBySlug, slug).
+		Scan(&res.Id, &res.Title, &res.Author, &res.Forum,
+			&res.Message, &res.Votes, &res.Slug, &res.Created); err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
